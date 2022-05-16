65110409388374e2a555cbbe9c8707de17b29de6
farzanekarami <50020784+Farzane-Ka@users.noreply.github.com>
2022-05-16 22:55:53 +0200

    Create App.js

---
+++ modal-logIn/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UserList/UsersList";
+function App() {
+    const [userList, setUserList] = useState([]);
+    const addUserHandler = (userName, age) => {
+        setUserList((prevUserList) => {
+            return [...prevUserList, {name: userName, age: age, id: Math.random().toString()}];
+        });
+    };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={userList}/>
+    </div>
+  );
+}
+
+export default App;
