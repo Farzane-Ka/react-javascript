05e3dce79bd026b3eff2d553b6efc30d59c7c26c
farzanekarami <50020784+Farzane-Ka@users.noreply.github.com>
2022-06-24 13:46:22 +0200

    Add files via upload

---
+++ Food-order-webpage/src/Layout/HeaderCartButton.js
@@ -0,0 +1,22 @@
+import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+import {useContext} from "react";
+import CartContext from "../../store/Cart-context";
+
+const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+    const numberofCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    },0);
+    return(
+        <button className={classes.button} onClick={props.onClick}>
+            <span className={classes.icon}>
+                <CartIcon/>
+            </span>
+            <span>Your cart</span>
+            <span className={classes.badge}>{numberofCartItems}</span>
+        </button>
+    )
+
+};
+export default HeaderCartButton;
